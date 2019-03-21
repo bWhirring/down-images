4d6191b494318756bb1ffc4a644d760824b688a1
huhu <bianlongting@dianwoda.com>
2019-03-21 12:51:11 +0800

    drop console

--- bin/index.js
+++ bin/index.js
@@ -1,25 +1,24 @@
-var Download = /** @class */ (function() {
-  function Download() {}
-  Download.prototype.image = function(url, name) {
-    var img = new Image();
-    img.setAttribute("crossOrigin", "Anonymous");
-    img.src = url;
-    img.onload = function() {
-      var width = img.width,
-        height = img.height;
-      console.log(width, height);
-      var canvas = document.createElement("canvas");
-      var context = canvas.getContext("2d");
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(img, 0, 0, width, height);
-      var a = document.createElement("a");
-      a.href = canvas.toDataURL();
-      a.download = name;
-      a.click();
+var Download = /** @class */ (function () {
+    function Download() {
+    }
+    Download.prototype.image = function (url, name) {
+        var img = new Image();
+        img.setAttribute("crossOrigin", "Anonymous");
+        img.src = url;
+        img.onload = function () {
+            var width = img.width, height = img.height;
+            var canvas = document.createElement("canvas");
+            var context = canvas.getContext("2d");
+            canvas.width = width;
+            canvas.height = height;
+            context.drawImage(img, 0, 0, width, height);
+            var a = document.createElement("a");
+            a.href = canvas.toDataURL();
+            a.download = name;
+            a.click();
+        };
     };
-  };
-  return Download;
-})();
+    return Download;
+}());
 module.exports = Download;
 //# sourceMappingURL=index.js.map
